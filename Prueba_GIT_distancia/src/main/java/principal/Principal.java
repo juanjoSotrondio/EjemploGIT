6397c17de4cbedb127fdbaf62019552b419dd8d3
Juanjo <Juanjo@EDUHBNQL73>
2023-11-28 17:28:42 +0100

    Primera semana de trabajo

--- Prueba_GIT_distancia/src/main/java/principal/Principal.java
+++ Prueba_GIT_distancia/src/main/java/principal/Principal.java
@@ -5,6 +5,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Comienzo de mi programa dificilísimo");
+		
+		System.out.println("Sigo con mi programa dificilísimo");
+		
+		System.out.println("Añado funcionalidades a programa dificilísimo");
+		
+		System.out.println("Estoy muy contento con mi programa dificilísimo");
+		
 	}
 
 }

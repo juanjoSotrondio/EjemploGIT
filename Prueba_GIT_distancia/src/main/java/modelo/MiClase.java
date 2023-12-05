7830b74322fc2fd6b0c23f4e4421512af44afadd
juanjoSotrondio <152307887+juanjoSotrondio@users.noreply.github.com>
2023-12-05 16:59:07 +0100

    Update domingo 2.java

--- Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
+++ Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
@@ -7,6 +7,7 @@ public class MiClase implements Miinterfaz{
 	@Override
 	public int metododevuelveentero() {
 		// TODO Auto-generated method stub
+		// No me dio tiempo de implementar este método....
 		return 0;
 	}
 

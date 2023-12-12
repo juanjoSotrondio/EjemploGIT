4ee11ede808ea4331271b5aaa37368c7899ed8d6
Juanjo <Juanjo@EDUHBNQL73>
2023-12-12 17:21:05 +0100

    Primer commit de la rama becario

---
+++ Prueba_GIT_distancia/src/main/java/modelo/Becario.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Becario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Trabajo duro del becario");
+		
+	}
+
+}
